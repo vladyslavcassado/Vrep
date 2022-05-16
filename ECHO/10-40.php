b7e1109fc19377b851b732c54201a722c9c3ca0f
vladyslavcassado <vladgrigorenko45@gmail.com>
2022-05-16 16:49:34 +0300

    a10 b40

---
+++ ECHO/10-40.php
@@ -0,0 +1,17 @@
+<?php
+$a = 10;
+$b = 40;
+echo '$a = ' . $a . PHP_EOL;
+echo '$b = ' . $b . PHP_EOL;
+
+echo PHP_EOL;
+echo $a = $a + $b;
+echo PHP_EOL;
+echo $b = $a - $b;
+echo PHP_EOL;
+echo $a = $a - $b;
+echo PHP_EOL;
+echo PHP_EOL;
+
+echo '$a = ' . $a . PHP_EOL;
+echo '$b = ' . $b . PHP_EOL;
