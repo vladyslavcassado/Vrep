3e94c8e7aaf2b79abd484ca37e6f05e646a298fa
vladyslavcassado <vladgrigorenko45@gmail.com>
2022-05-19 20:29:33 +0300

    Create 09.php

---
+++ ДЗ 10. Пальці/09.php
@@ -0,0 +1,21 @@
+<?php
+
+$finger = random_int(1, 5);
+switch ($finger) {
+    case 1;
+           echo 'Большой палец';
+    break;
+    case 2;
+           echo 'Указательный палец';
+    break;
+    case 3;
+           echo 'Средний палец';
+    break;
+    case 4;
+           echo 'Безимянный палец';
+    break;
+    case 5;
+           echo 'Мезинец';
+    break;
+}
+
